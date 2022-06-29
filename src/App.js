90536477f6e0c62d7f32f2e9ee7ed7fa19e49f1c
Akshay Sharama <codewithakki@gmail.com>
2022-06-29 16:13:24 +0000

    addedwizard

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import Billing from "./pages/Billing";
 // import Rtl from "./pages/Rtl";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
+import FormWizard from './pages/FormWizard'
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
@@ -31,6 +32,7 @@ function App() {
         <Main>
           <Route exact path="/dashboard" component={Home} />
           <Route exact path="/tables" component={Tables} />
+          <Route exact path="/wizard" component={FormWizard} />
           <Route exact path="/billing" component={Billing} />
           {/* <Route exact path="/rtl" component={Rtl} /> */}
           <Route exact path="/profile" component={Profile} />
---
+++ src/pages/FormWizard.js
@@ -0,0 +1,69 @@
+import {Button,message,Steps} from "antd";
+import { useState} from "react";
+
+
+const { Step } = Steps;
+const steps = [
+  {
+    title: 'First',
+    content: 'First-content',
+  },
+  {
+    title: 'Second',
+    content: 'Second-content',
+  },
+  {
+    title: 'Last',
+    content: 'Last-content',
+  },
+];
+
+const FormWizard = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  return (
+    <>
+      <Steps current={current}>
+        {steps.map((item) => (
+          <Step key={item.title} title={item.title} />
+        ))}
+      </Steps>
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button
+            style={{
+              margin: '0 8px',
+            }}
+            onClick={() => prev()}
+          >
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FormWizard;
+
+
+/* ReactDOM.render(<ComponentDemo />, mountNode); */
