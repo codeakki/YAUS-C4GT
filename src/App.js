57f73697f91f53b6c6f7370797a5d66c0fa82de0
codeakki <codewithakki@gmail.com>
2022-07-02 16:29:32 +0530

    Working Steps for Link Creation

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import SignUp from "./pages/SignUp";
 import FormWizard from './pages/FormWizard'
 import SignIn from "./pages/SignIn";
 import Main from "./components/layout/Main";
+
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
@@ -29,8 +30,9 @@ function App() {
       <Switch>
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" exact component={SignIn} />
+        {/*  <Route exact path="/first" component={First} /> */}
         <Main>
-          <Route exact path="/wizard" component={FormWizard} />
+          <Route exact path="/LinkCreate" component={FormWizard} />
           <Route exact path="/dashboard" component={Home} />
           <Route exact path="/tables" component={Tables} />
           <Route exact path="/billing" component={Billing} />
